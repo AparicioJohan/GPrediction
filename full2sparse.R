dfaee0f1bfc88d6c4b327f9d171cdef406a9d87b
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2020-03-10 08:26:57 -0500

    Add files via upload

---
+++ full2sparse.R
@@ -0,0 +1,17 @@
+
+library(data.table)
+
+full2sparse <- function(mat) {
+  if (is.null(colnames(mat))) {
+   colnames(mat) <- 1:ncol(mat)
+  }
+  regions <- as.numeric(colnames(mat))
+  sparse <- as.data.table(expand.grid(regions, regions))
+  up.triangle <- c(upper.tri(mat, diag = TRUE))
+  sparse <- cbind(sparse, c(mat), up.triangle)
+  colnames(sparse) <- c("region1", "region2", "IF", "up.triangle")
+  sparse <- subset(sparse, up.triangle == TRUE & IF != 0)
+  sparse <- sparse[, 1:3, with = FALSE]
+  sparse <- sparse[,c(2,1,3)]
+  return(sparse)
+}
